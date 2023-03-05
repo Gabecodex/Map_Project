791f983c0c434bac54e59dff4fc9f3ee44c584f6
gabecodex <gabrielfmdesouza@gmail.com>
2023-03-05 17:48:28 -0500

    app android

---
+++ app.java
@@ -0,0 +1,14 @@
+import android.os.Handler;
+import android.os.Looper;
+import android.os.Message;
+import android.util.Log;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+public class MyService extends Service {
